18401ed984d67e6b6e1f340a305e5759dba03d5c
night-slayer18 <samanuaia257@gmail.com>
2025-08-03 13:22:42 +0530

    updated version

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 )
 
 // version
-var version = "1.0.0"
+var version = "1.1.0"
 // rootCmd represents the base command when called without any subcommands.
 var rootCmd = &cobra.Command{
 	Use:   "goforge",
